bbe9728f1f22337afbe6a838da70e9bdade99178
renancvitor <renan.vitor.cm@gmail.com>
2025-04-02 23:22:14 -0300

    Convertendo valores

--- primeiroProgramaAlura/src/Main.java
+++ primeiroProgramaAlura/src/Main.java
@@ -21,5 +21,9 @@ public class Main {
                 """ + anoLancamento;
         System.out.println(sinopse);
 
+        int classificacao;
+        classificacao = (int) (media / 2);
+        System.out.println(classificacao);
+
     }
 }
