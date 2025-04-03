8fe1f74f2e9c209c32141987de26e8cfc06e9db6
renancvitor <renan.vitor.cm@gmail.com>
2025-04-03 07:33:28 -0300

    Scanner

---
+++ primeiroProgramaAlura/src/Leitura.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito:");
+        String filme = scanner.nextLine();
+        System.out.println(filme);
+
+        System.out.println("Qual o ano de lançamento:");
+        int anoLancamento = scanner.nextInt();
+        System.out.println(anoLancamento);
+
+        System.out.println("Digite a sua avaliação do filme:");
+        double nota = scanner.nextDouble();
+        System.out.println(nota);
+    }
+}
