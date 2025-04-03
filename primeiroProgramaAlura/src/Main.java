b3fcb1c3e8c66fa016dbcb882534a13f25cb199b
renancvitor <renan.vitor.cm@gmail.com>
2025-04-02 23:07:21 -0300

    Atribuição de valores

--- primeiroProgramaAlura/src/Main.java
+++ primeiroProgramaAlura/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoLancamento);
+
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
     }
 }
