b13eb3d5ef10486d3efd90dad7beb0786ad3b0c2
renancvitor <renan.vitor.cm@gmail.com>
2025-04-02 23:18:54 -0300

    Strings

--- primeiroProgramaAlura/src/Main.java
+++ primeiroProgramaAlura/src/Main.java
@@ -9,5 +9,17 @@ public class Main {
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
 
+        double media = (9.8 + 6.3 + 8.8) / 3;
+        System.out.println(String.format("%.2f", media));
+
+        String sinopse;
+        sinopse = """
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoLancamento;
+        System.out.println(sinopse);
+
     }
 }
