759f9d08a1400a6a760ae5df21d510da4bf2c538
renancvitor <renan.vitor.cm@gmail.com>
2025-04-04 19:03:30 -0300

    Trabalhando com getters and setters

--- screenmatchJava-filme/src/Program/Principal.java
+++ screenmatchJava-filme/src/Program/Principal.java
@@ -3,9 +3,10 @@ package Program;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso Chefão";
-        meuFilme.anoLancamento = 1970;
-        meuFilme.duracaoMinutos = 180;
+        meuFilme.setNome("O Poderoso Chefão");
+        meuFilme.setIncluidoNoPlano(true);
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracaoMinutos(180);
 
         meuFilme.exibeFechaTecnica();
 
@@ -14,5 +15,7 @@ public class Principal {
         meuFilme.avalia(5);
         System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.printf("Média avaliações: " + "%.2f%n", meuFilme.mediaAvaliacoes());
+
+        System.out.println(meuFilme.toString());
     }
 }
