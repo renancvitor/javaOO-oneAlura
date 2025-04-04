29457035db9ba08087ae9472f821d77bd5b3c534
renancvitor <renan.vitor.cm@gmail.com>
2025-04-04 07:24:18 -0300

    Preparação classe

---
+++ screenmatchJava-filme/src/Program/Filme.java
@@ -0,0 +1,12 @@
+package Program;
+
+public class Filme {
+
+    String nome;
+    int anoLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalAvaliacoes;
+    int duracaoMinutos;
+
+}
