bb232621c877cf35c61a973c1364f104571a2626
renancvitor <renan.vitor.cm@gmail.com>
2025-04-07 19:21:00 -0300

    Diferentes tipos de objetos na lista

---
+++ listasEColecoesDeDados/src/Program/PrincipalComListas.java
@@ -0,0 +1,32 @@
+package Program;
+
+import modelos.Filme;
+import modelos.Serie;
+import modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        meuFilme.avalia(9);
+        Serie peakyBlinders = new Serie("Peaky Blinders", 2020);
+        peakyBlinders.avalia(10);
+        Filme outroFilme = new Filme("Outro Filme", 2025);
+        outroFilme.avalia(6);
+        Filme filmeRenan = new Filme("Todo mundo em Pânico 1", 2003);
+        filmeRenan.avalia(9);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeRenan);
+        lista.add(outroFilme);
+        lista.add(meuFilme);
+        lista.add(peakyBlinders);
+
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classificação " + filme.getClassificacao());
+        }
+    }
+}
