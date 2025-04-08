de6d893b4c6edfe59b6df345d208d8c74864b860
renancvitor <renan.vitor.cm@gmail.com>
2025-04-07 22:51:38 -0300

    Identificando objeto

--- listasEColecoesDeDados/src/Program/PrincipalComListas.java
+++ listasEColecoesDeDados/src/Program/PrincipalComListas.java
@@ -25,8 +25,9 @@ public class PrincipalComListas {
 
         for (Titulo item: lista) {
             System.out.println(item.getNome());
-            Filme filme = (Filme) item;
-            System.out.println("Classificação " + filme.getClassificacao());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
         }
     }
 }
