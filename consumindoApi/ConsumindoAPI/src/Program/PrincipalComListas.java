995fff1620a8dbe2db9edf216e1001cbcbae5818
renancvitor <renan.vitor.cm@gmail.com>
2025-04-11 19:48:44 -0300

    Preparação curso Consumindo API Java

---
+++ consumindoApi/ConsumindoAPI/src/Program/PrincipalComListas.java
@@ -0,0 +1,50 @@
+package Program;
+
+import modelos.Filme;
+import modelos.Serie;
+import modelos.Titulo;
+
+import java.util.*;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso Chefão", 1970);
+        meuFilme.avalia(9);
+        Serie peakyBlinders = new Serie("Peaky Blinders", 2020);
+        peakyBlinders.avalia(10);
+        Filme outroFilme = new Filme("Outro Filme", 2025);
+        outroFilme.avalia(6);
+        Filme filmeRenan = new Filme("Todo mundo em Pânico 1", 2003);
+        filmeRenan.avalia(9);
+
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filmeRenan);
+        lista.add(outroFilme);
+        lista.add(meuFilme);
+        lista.add(peakyBlinders);
+
+        for (Titulo item: lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo");
+        buscaPorArtista.add("Jaqueline");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação:");
+        System.out.println(buscaPorArtista);
+
+        System.out.println("Lista de títulos ordenados:");
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getNome));
+        System.out.println("Ordenando por nome:");
+        System.out.println(lista);
+    }
+}
---
+++ consumindoApi/ConsumindoAPI/src/modelos/Titulo.java
@@ -0,0 +1,83 @@
+package modelos;
+
+public class Titulo implements Comparable<Titulo> {
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoNoPlano;
+    private double somaAvaliacao;
+    private int totalAvaliacoes;
+    private int duracaoMinutos;
+
+    public Titulo(String nome, int anoLancamento) {
+        this.nome = nome;
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getTotalAvaliacoes() {
+        return totalAvaliacoes;
+    }
+
+    public int getDuracaoMinutos() {
+        return duracaoMinutos;
+    }
+
+    public void setDuracaoMinutos(int duracaoMinutos) {
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public void exibeFechaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Duração do filme: " + duracaoMinutos);
+        System.out.println("Ano de lançamento: " + anoLancamento);
+    }
+
+    public void avalia(double nota) {
+        somaAvaliacao += nota;
+        totalAvaliacoes++;
+    }
+
+    public double mediaAvaliacoes() {
+        return somaAvaliacao / totalAvaliacoes;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", anoLancamento=" + anoLancamento +
+                ", incluidoNoPlano=" + incluidoNoPlano +
+                ", somaAvaliacao=" + somaAvaliacao +
+                ", totalAvaliacoes=" + totalAvaliacoes +
+                ", duracaoMinutos=" + duracaoMinutos +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+}
