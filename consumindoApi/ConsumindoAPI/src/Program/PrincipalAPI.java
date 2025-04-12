605f35957dc77043b86be9f8b0ffbdf6b6401d9f
renancvitor <renan.vitor.cm@gmail.com>
2025-04-12 00:06:45 -0300

    Primeiro contato com códigos de API

---
+++ consumindoApi/ConsumindoAPI/src/Program/PrincipalAPI.java
@@ -0,0 +1,19 @@
+package Program;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalAPI {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://www.omdbapi.com/?apikey=1653b8ce&s=Divertidamente"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
