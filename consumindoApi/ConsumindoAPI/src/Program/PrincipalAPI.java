0117ba0de5fa51e247040ef5977db19e5c9fd6d9
renancvitor <renan.vitor.cm@gmail.com>
2025-04-14 13:27:25 -0300

    Criando maneira interativa de acessar a API

--- consumindoApi/ConsumindoAPI/src/Program/PrincipalAPI.java
+++ consumindoApi/ConsumindoAPI/src/Program/PrincipalAPI.java
@@ -5,15 +5,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalAPI {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para buscar: ");
+        String buscaFilme = sc.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?apikey=1653b8ce&s=" + buscaFilme;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?apikey=1653b8ce&s=Divertidamente"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+        
     }
 }
