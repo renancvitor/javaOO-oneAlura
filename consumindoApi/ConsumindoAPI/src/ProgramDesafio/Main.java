a8ab37ad88bc20e30febdaa8f35b3924d27ef7f0
renancvitor <renan.vitor.cm@gmail.com>
2025-04-15 23:18:52 -0300

    Criação da lista de endereços

--- consumindoApi/ConsumindoAPI/src/ProgramDesafio/Main.java
+++ consumindoApi/ConsumindoAPI/src/ProgramDesafio/Main.java
@@ -8,35 +8,49 @@ import exceptionsDesafio.ErroDeConversaoDeCepException;
 import serviceDesafio.ViaCepService;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        String cep = "";
         Gson gson = GsonConfig.criarGson();
 
         ViaCepService viaCepService = new ViaCepService();
 
-        System.out.print("\nDigite o CEP que deseja buscar(somente números):  ");
-        String cep = sc.nextLine();
+        List<Endereco> enderecos = new ArrayList<>();
 
-        try {
-            String json = viaCepService.buscarEndereco(cep);
+        while (!cep.equalsIgnoreCase("sair")) {
+            System.out.print("\nDigite o CEP que deseja buscar(somente números):  ");
+            cep = sc.nextLine();
+
+            if (cep.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            try {
+                String json = viaCepService.buscarEndereco(cep);
 //            System.out.println("Resposta da API: " + json);
 
-            EnderecoViaCep enderecoViaCep = gson.fromJson(json, EnderecoViaCep.class);
+                EnderecoViaCep enderecoViaCep = gson.fromJson(json, EnderecoViaCep.class);
 
-            Endereco endereco = new Endereco(enderecoViaCep);
+                Endereco endereco = new Endereco(enderecoViaCep);
 
-            System.out.println("Endereço Encontrado: " + endereco);
-        } catch (ErroDeConversaoDeCepException e) {
-            System.out.println("Erro de conversão do CEP: " + e.getMensagem());
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Erro ao buscar o endereço: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Erro inesperado: " + e.getMessage());
+                System.out.println("Endereço Encontrado: " + endereco);
+
+                enderecos.add(endereco);
+
+            } catch (ErroDeConversaoDeCepException e) {
+                System.out.println("Erro de conversão do CEP: " + e.getMensagem());
+            } catch (IOException | InterruptedException e) {
+                System.out.println("Erro ao buscar o endereço: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Erro inesperado: " + e.getMessage());
+            }
         }
+        System.out.println(enderecos);
 
         sc.close();
     }
---
+++ consumindoApi/ConsumindoAPI/src/serviceDesafio/EnderecoService.java
@@ -0,0 +1,5 @@
+package serviceDesafio;
+
+public class EnderecoService {
+
+}
